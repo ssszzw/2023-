4fcfe92c2c7be8f71970927c644e34b2658238e1
Szw-local <2582142171@qq.com>
2023-11-19 19:27:25 +0800

    修改名字

--- deploy.sh
+++ deploy.sh
@@ -44,7 +44,7 @@ echo -e "开始构建 jar 包..."
 
 docker run -it --rm --name ssadmin-maven \
     -v ssadmin-maven-repo:/root/.m2 \
-    -v "$PWD/todo_3":/usr/src/mymaven \
+    -v "$PWD/RBAC-server":/usr/src/mymaven \
     -w /usr/src/mymaven \
     maven:3.8.4-jdk-8 mvn clean install package -e '-Dmaven.test.skip=true' || { echo -e "${RED}构建 jar 包失败。${NC}"; exit 1; }
 
